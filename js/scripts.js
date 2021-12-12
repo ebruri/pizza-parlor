d5f5df3c75e3a608dcf26ffdcc3ff1a52e65c092
Ebru Rice <ebruerenb@gmail.com>
2021-12-11 22:30:45 -0800

    Add totalCost prototype to calculate the pizza price

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,11 @@ function Pizza(toppings, size, crust) {
   this.size =size;
   this.crust = crust;
 }
+Pizza.prototype.totalCost = function(){
+  let result = 0;
+  for (let i = 0; i < this.toppings.length - 1; i++) {
+    result += this.toppings[i];
+  }
+  return result + this.size + this.crust;
+}
+
